480a8ccc5424d22cd6d888d9eff209f2b00a084d
Dalvis <Dalvis.liz27@gmail.com>
2024-08-03 19:39:09 -0400

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+/* eslint-disable */
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
